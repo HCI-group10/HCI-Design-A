a81e73a38a266ffee883dd8a25376ebf24990628
VBoudreau55 <vancebou55@gmail.com>
2023-10-26 01:03:43 -0400

    delete unnecessary items

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,30 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: '', // This is the launch page
-        name: 'LaunchPage',
-        component: () => import(/* webpackChunkName: "launch" */ '@/views/LaunchPage.vue'),
+        path: "", // This is the launch page
+        name: "LaunchPage",
+        component: () =>
+          import(/* webpackChunkName: "launch" */ "@/views/LaunchPage.vue"),
       },
       {
-        path: 'home', // This is the home page nested under the launch page
-        name: 'Home',
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-      {
-        path: 'table-item/:id', // This is the table item navigation page with a dynamic route parameter
-        name: 'TableItem',
-        component: () => import(/* webpackChunkName: "table-item" */ '@/views/TableItem.vue'),
+        path: "home", // This is the home page nested under the launch page
+        name: "Home",
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
       },
     ],
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
-
+export default router;
